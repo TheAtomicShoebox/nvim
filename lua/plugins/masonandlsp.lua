c774e9135736b92b112b8af09f80f33432154003
Adam Schubach <33351047+TheAtomicShoebox@users.noreply.github.com>
2025-05-24 01:07:48 -0400

    Removing unnecessary plugins/config

--- lua/plugins/masonandlsp.lua
+++ lua/plugins/masonandlsp.lua
@@ -3,27 +3,20 @@ if vim.g.vscode then
 end
 
 return {
-  -- {
-  --   "williamboman/mason.nvim",
-  --   dependencies = {
-  --     "mason-org/mason-registry",
-  --     "Crashdummyy/mason-registry",
-  --   },
-  -- },
   {
-    "neovim/nvim-lspconfig",
+    'neovim/nvim-lspconfig',
     dependencies = {
       {
-        "williamboman/mason.nvim",
+        'williamboman/mason.nvim',
         opts = {
           registries = {
-            "github:mason-org/mason-registry",
-            "github:Crashdummyy/mason-registry",
+            'github:mason-org/mason-registry',
+            'github:Crashdummyy/mason-registry',
           },
         },
       },
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-      "williamboman/mason-lspconfig.nvim",
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
+      'williamboman/mason-lspconfig.nvim',
     },
   },
 }
