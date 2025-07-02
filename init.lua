1ef44554431a7d67dba11e49353d8494516bd5a7
Adam Schubach <33351047+TheAtomicShoebox@users.noreply.github.com>
2025-07-02 12:20:10 -0400

    Burning everything and restarting
    Forcibly reconfiguring everything for vscode so nothing is likely to
    randomly fail later as an oversight
    
    Ultimately undone as I continue to use LazyVim for vscode

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.vscode then
+
+else
+    --[[
+        if the flag isn't operating properly, then this will break the installation -- intentionally
+        this also allows me to ensure the eventual rebase of this onto the main branch will continue to operate
+        this can also potentially allow me to create 2 separate streams: normal config and vs config, within whatever package manager I use
+        Thus, checking the flag this early is a pleasant setup
+    ]]
+    require ('config.lazy') 
+end
