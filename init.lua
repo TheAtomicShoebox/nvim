5e4aaa2170b16cc2708e3f062d02e3d16337c9ec
Adam Schubach <33351047+TheAtomicShoebox@users.noreply.github.com>
2025-07-07 17:18:00 -0400

    Combing vscode and main neovim configurations

--- init.lua
+++ init.lua
@@ -1,16 +1,8 @@
-local vsCodeIsEnabled = vim.g.vscode and 'VS Code Enabled' or 'VS Code Disabled'
-
 if vim.g.vscode then
     local vscode = require 'vscode'
     vim.notify = vscode.notify
-else
-    --[[
-        if the flag isn't operating properly, then this will break the installation -- intentionally
-        this also allows me to ensure the eventual rebase of this onto the main branch will continue to operate
-        this can also potentially allow me to create 2 separate streams: normal config and vs config, within whatever package manager I use
-        Thus, checking the flag this early is a pleasant setup
-    ]]
-    require 'config.lazy'
+    vim.notify('VS Code Enabled')
 end
+    require 'config.lazy'
 
-vim.notify(vsCodeIsEnabled, vim.log.levels.INFO)
+Snacks.notifier.notify('VS Code Disabled')
